26a6eba4d077cf703b60b88522cda6e6e43bf5ca
pinaki78 <39923626+pinaki78@users.noreply.github.com>
2018-06-05 06:57:21 +0300

    Update addDeviceToIotCatalog.js

--- addDeviceToIotCatalog.js
+++ addDeviceToIotCatalog.js
@@ -3,7 +3,6 @@ var AWS = require('aws-sdk');
 var dynamo = new AWS.DynamoDB.DocumentClient();
 var table = "iotCatalog";
 
-//Lambda function-1
 
 exports.handler = function(event, context) {
     //console.log('Received event:', JSON.stringify(event, null, 2));
